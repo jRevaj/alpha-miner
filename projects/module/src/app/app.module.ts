b1f153affc94b53ed03ae50b19bcddd4350ec8ab
Juraj Revaj <59011895+jRevaj@users.noreply.github.com>
2022-10-18 13:27:03 +0200

    Initial commit

---
+++ projects/module/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {IlpnComponentsModule} from 'ilpn-components';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {APP_BASE_HREF, PlatformLocation} from '@angular/common';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        IlpnComponentsModule,
+        FlexLayoutModule,
+        ReactiveFormsModule,
+        MatSlideToggleModule,
+        BrowserAnimationsModule,
+    ],
+    providers: [
+        {
+            provide: APP_BASE_HREF,
+            useFactory: (s: PlatformLocation) => s.getBaseHrefFromDOM(),
+            deps: [PlatformLocation]
+        }
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
