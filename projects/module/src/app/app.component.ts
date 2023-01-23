7903ab568eac116516d7794b7d9973fd6c8fb4de
Juraj Revaj <59011895+jRevaj@users.noreply.github.com>
2023-01-23 11:15:23 +0100

    v2: find all events in log, find starting events, find ending events

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,12 +1,55 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {
+    DropFile, FD_LOG,
+    FD_PETRI_NET,
+} from 'ilpn-components';
+import {AlphaMinerService} from "./services/alpha-miner.service";
+import {Subscription} from "rxjs";
+import {Trace} from "../../../components/src/lib/models/log/model/trace";
+import {XesLogParserService} from "../../../components/src/lib/models/log/parser/xes-log-parser.service";
+import {
+    PetriNetSerialisationService
+} from "../../../components/src/lib/models/pn/parser/petri-net-serialisation.service";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-    constructor() {
+export class AppComponent implements OnDestroy {
+    public fdLog = FD_LOG;
+    public fdPN = FD_PETRI_NET;
+
+    public log: Array<Trace> | undefined;
+    public pnResult: DropFile | undefined = undefined;
+    public reportResult: DropFile | undefined = undefined;
+    public processing = false;
+
+    private _sub: Subscription | undefined;
+
+    constructor(private _logParser: XesLogParserService,
+                private _netSerializer: PetriNetSerialisationService,
+                private _miner: AlphaMinerService) {
+    }
+
+    ngOnDestroy(): void {
+        this._sub?.unsubscribe()
+    }
+
+    public processLog(files: Array<DropFile>) {
+        this.processing = true;
+        this.pnResult = undefined;
+
+        this.log = this._logParser.parse(files[0].content);
+        console.debug(this.log);
+
+        const runs = `number of traces: ${this.log.length}`;
+
+        const start = performance.now();
+
+        this._miner.mine(this.log);
+
+        const stop = performance.now();
     }
 
 }
