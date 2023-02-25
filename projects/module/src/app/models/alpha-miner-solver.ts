815e85318831694218a9d6a6d87a843e29376da5
Juraj Revaj <59011895+jRevaj@users.noreply.github.com>
2023-02-25 16:28:01 +0100

    alpha-miner-solver: alpha-miner.service refactioring + construction of final petri-net v1

---
+++ projects/module/src/app/models/alpha-miner-solver.ts
@@ -0,0 +1,212 @@
+import {Injectable} from '@angular/core';
+import * as _ from 'lodash';
+import {Trace} from "../../../../components/src/lib/models/log/model/trace";
+import {Footprint} from "./footprint";
+import {LoopLengthOne} from "./loop-length-one";
+import {Place} from "../../../../components/src/lib/models/pn/model/place";
+import {containsAll, setOfAllSubsets} from "./utility";
+import {PetriNet} from "../../../../components/src/lib/models/pn/model/petri-net";
+import {Transition} from "../../../../components/src/lib/models/pn/model/transition";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AlphaMinerSolver {
+    public loopsL2: boolean = true;
+
+    constructor() {
+    }
+
+    public discoverWFNet(log: Array<Trace>): PetriNet {
+        let loopsL1: Set<LoopLengthOne> = new Set();
+        for (const trace of log) {
+            this.processLoopsL1(trace, loopsL1);
+        }
+        console.log("loops of length one: ", loopsL1);
+
+        let eventList: Set<string> = new Set();
+        let startingEvents: Set<string> = new Set();
+        let endingEvents: Set<string> = new Set();
+
+        // find list of all events & starting events & ending events
+        this.extractEvents(log, eventList, startingEvents, endingEvents);
+        console.log("eventList: ", eventList);
+        console.log("startingEvents: ", startingEvents);
+        console.log("endingEvents: ", endingEvents);
+
+        // generate matrix of relations from log
+        const footprint: Footprint = new Footprint(eventList, log, true);
+        console.log("footprint matrix: ", footprint.footprint);
+
+        // generate xl set
+        let xl: Set<Place> = this.generatePlacesFromFootprint(footprint, eventList);
+
+        // generate yl set by reducing xl set
+        let yl: Array<string> = this.reduceXl(xl);
+
+        // TODO: take in account discovered loops of length 1
+        // this.postProcessLoopsL1(loopsL1, yl, eventList);
+
+        return this.constructPetriNet(eventList, yl);
+    }
+
+    private extractEvents(eventLog: Array<Trace>, allEvents: Set<string>, startingEvents: Set<string>, endingEvents: Set<string>): void {
+        allEvents.clear();
+        startingEvents.clear();
+        endingEvents.clear();
+
+        for (const trace of eventLog) {
+            startingEvents.add(trace.get(0));
+            endingEvents.add(trace.get(trace.length() - 1));
+            trace.eventNames.forEach(event => allEvents.add(event));
+        }
+    }
+
+    private checkL1(eventsInTrace: Array<string>) {
+        for (let i = 0; i < eventsInTrace.length - 1; i++) {
+            if (_.isEqual(eventsInTrace[i], eventsInTrace[i+1])) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private processLoopsL1(trace: Trace, loopsL1: Set<LoopLengthOne>) {
+        // TODO: test method if working correctly
+        let start;
+        let eventsInTrace: Array<string> = trace.eventNames;
+        while ((start = this.checkL1(eventsInTrace)) != -1) {
+            let prev: number = start - 1;
+            let currentEvent: number = start;
+            while (currentEvent < eventsInTrace.length - 1 && _.isEqual(eventsInTrace[currentEvent], eventsInTrace[currentEvent + 1])) {
+                currentEvent++;
+            }
+
+            currentEvent++;
+            let loop: LoopLengthOne = new LoopLengthOne(eventsInTrace[prev], eventsInTrace[start], eventsInTrace[currentEvent]);
+            let numOfLoopedEvents: number = currentEvent - start;
+            for (let i = 0; i < numOfLoopedEvents; i++) {
+                eventsInTrace.splice(start, 1);
+            }
+
+            loopsL1.add(loop);
+        }
+    }
+
+    private generatePlacesFromFootprint(footprint: Footprint, eventList: Set<string>): Set<Place> {
+        let xl: Set<any> = new Set();
+        let pSet: Set<any> = setOfAllSubsets(eventList);
+        console.log("size of pSet: ", pSet.size);
+
+        // filter out subsets with empty value
+        pSet.forEach((e: any) => {
+            if (e.size == 0) {
+                pSet.delete(e);
+            }
+        });
+
+        // convert to arr
+        let pSetArr: Array<any> = _.toArray(pSet);
+
+        for (let i = 0; i < pSetArr.length; i++) {
+            let first: Set<string> = pSetArr[i];
+            for (let j = 0; j < pSetArr.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                let second: Set<string> = pSetArr[j];
+                if (footprint.areEventsConnected(first, second)) {
+                    let toAdd: Set<string>[] = [first, second];
+                    xl.add(toAdd)
+                }
+            }
+        }
+        console.log("size of xl: ", xl.size);
+        return xl;
+    }
+
+    private reduceXl(xl: Set<Place>): Array<string> {
+        let toRemove: Set<any> = new Set();
+        let potential: Array<any> = _.toArray(xl);
+
+        for (let i = 0; i < potential.length - 1; i++) {
+            let iPotential: Set<string>[] = potential[i];
+            let iInEvents: any = _.toArray(iPotential[0]);
+            let iOutEvents: any = _.toArray(iPotential[1]);
+
+            for (let j = i + 1; j < potential.length; j++) {
+                let jPotential: Set<string>[] = potential[j];
+                let jInEvents: any = _.toArray(jPotential[0]);
+                let jOutEvents: any = _.toArray(jPotential[1]);
+                if (containsAll(iInEvents, jInEvents)) {
+                    if (containsAll(iOutEvents, jOutEvents)) {
+                        toRemove.add(jPotential);
+                        continue;
+                    }
+                }
+
+                if (containsAll(jInEvents, iInEvents)) {
+                    if (containsAll(jOutEvents, iOutEvents)) {
+                        toRemove.add(iPotential);
+                    }
+                }
+            }
+        }
+
+        let toRemoveArr: Array<any> = _.toArray(toRemove);
+        let yl: Array<any> = _.difference(potential, toRemoveArr);
+        console.log("yl set: ", yl);
+        return yl;
+    }
+
+    private postProcessLoopsL1(loopsL1: Set<LoopLengthOne>, yl: Array<string>, eventList: Set<string>): void {
+        // TODO: finish loops processing
+
+
+    }
+
+    private constructPetriNet(eventList: Set<string>, yl: Array<string>): PetriNet {
+        // TODO: debug construction (seems to be reversed)
+        const net: PetriNet = new PetriNet();
+
+        eventList.forEach(event => {
+            const t: Transition = new Transition(event, 0, 0, event);
+            net.addTransition(t);
+        })
+
+        yl.forEach((mapping, index) => {
+            const p: Place = new Place(0, 0, 0, "p" + (index + 1));
+            net.addPlace(p);
+            for (const inEvent of mapping[0]) {
+                const t: Transition | undefined = net.getTransition(inEvent);
+                if (t) {
+                    net.addArc(p, t, 1);
+                }
+            }
+            for (const outEvent of mapping[1]) {
+                const t: Transition | undefined = net.getTransition(outEvent);
+                if (t) {
+                    net.addArc(t, p, 1);
+                }
+            }
+        })
+
+        for (const t of net.getTransitions()) {
+            if (t.ingoingArcs.length === 0) {
+                const p = new Place(1, 0, 0, "in");
+                net.addPlace(p);
+                net.addArc(p, t);
+            }
+            if (t.outgoingArcs.length === 0) {
+                const p = new Place(0, 0, 0, "out");
+                net.addPlace(p);
+                net.addArc(t, p);
+            }
+        }
+
+        console.log("solution pn: ", net);
+        return net;
+    }
+}
--- projects/module/src/app/services/alpha-miner.service.ts
+++ projects/module/src/app/services/alpha-miner.service.ts
@@ -1,11 +1,11 @@
 import {Injectable} from '@angular/core';
-import * as _ from 'lodash';
 import {cleanLog} from "../../../../components/src/lib/algorithms/log/clean-log";
 import {Trace} from "../../../../components/src/lib/models/log/model/trace";
-import {Footprint} from "../models/footprint";
-import {LoopLengthOne} from "../models/loop-length-one";
-import {Place} from "../../../../components/src/lib/models/pn/model/place";
-import {containsAll, setOfAllSubsets} from "../models/utility";
+import {
+    DuplicatePlaceRemoverService
+} from "../../../../components/src/lib/algorithms/pn/transformation/duplicate-place-remover.service";
+import {NetAndReport} from "../../../../components/src/lib/algorithms/pn/synthesis/model/net-and-report";
+import {AlphaMinerSolver} from "../models/alpha-miner-solver";
 
 @Injectable({
     providedIn: 'root'
@@ -13,150 +13,15 @@ import {containsAll, setOfAllSubsets} from "../models/utility";
 export class AlphaMinerService {
     public loopsL2: boolean = true;
 
-    constructor() {
+    constructor(private _duplicatePlaceRemover: DuplicatePlaceRemoverService) {
     }
 
-    public mine(log: Array<Trace>) {
+    public mine(log: Array<Trace>): NetAndReport {
         const cleanedLog = cleanLog(log);
-        this.discoverWFNet(cleanedLog);
-    }
-
-    private discoverWFNet(log: Array<Trace>) {
-        let loopsL1: Set<LoopLengthOne> = new Set();
-        for (const trace of log) {
-            this.processLoopsL1(trace, loopsL1);
+        const alphaSolver = new AlphaMinerSolver();
+        return {
+            net: this._duplicatePlaceRemover.removeDuplicatePlaces(alphaSolver.discoverWFNet(cleanedLog)),
+            report: ["test"]
         }
-        console.log("loops of length one: ", loopsL1);
-
-        let eventList: Set<string> = new Set();
-        let startingEvents: Set<string> = new Set();
-        let endingEvents: Set<string> = new Set();
-
-        // find list of all events & starting events & ending events
-        this.extractEvents(log, eventList, startingEvents, endingEvents);
-        console.log("eventList: ", eventList);
-        console.log("startingEvents: ", startingEvents);
-        console.log("endingEvents: ", endingEvents);
-
-        // generate matrix of relations from log
-        const footprint: Footprint = new Footprint(eventList, log, true);
-        console.log("footprint matrix: ", footprint.footprint);
-
-        // generate xl set
-        let xl: Set<Place> = this.generatePlacesFromFootprint(footprint, eventList);
-
-        // generate yl set by reducing xl set
-        let yl: Array<string> = this.reduceXl(xl);
-    }
-
-    private extractEvents(eventLog: Array<Trace>, allEvents: Set<string>, startingEvents: Set<string>, endingEvents: Set<string>): void {
-        allEvents.clear();
-        startingEvents.clear();
-        endingEvents.clear();
-
-        for (let trace of eventLog) {
-            startingEvents.add(trace.get(0));
-            endingEvents.add(trace.get(trace.length() - 1));
-            trace.eventNames.forEach(event => allEvents.add(event));
-        }
-    }
-
-    private checkL1(eventsInTrace: Array<string>) {
-        for (let i = 0; i < eventsInTrace.length - 1; i++) {
-            if (_.isEqual(eventsInTrace[i], eventsInTrace[i+1])) {
-                return i;
-            }
-        }
-
-        return -1;
-    }
-
-    private processLoopsL1(trace: Trace, loopsL1: Set<LoopLengthOne>) {
-        // TODO: test method if working correctly
-        let start;
-        let eventsInTrace: Array<string> = trace.eventNames;
-        while ((start = this.checkL1(eventsInTrace)) != -1) {
-            let prev: number = start - 1;
-            let currentEvent: number = start;
-            while (currentEvent < eventsInTrace.length - 1 && _.isEqual(eventsInTrace[currentEvent], eventsInTrace[currentEvent + 1])) {
-                currentEvent++;
-            }
-
-            currentEvent++;
-            let loop: LoopLengthOne = new LoopLengthOne(eventsInTrace[prev], eventsInTrace[start], eventsInTrace[currentEvent]);
-            let numOfLoopedEvents: number = currentEvent - start;
-            for (let i = 0; i < numOfLoopedEvents; i++) {
-                eventsInTrace.splice(start, 1);
-            }
-
-            loopsL1.add(loop);
-        }
-    }
-
-    private generatePlacesFromFootprint(footprint: Footprint, eventList: Set<string>): Set<Place> {
-        let xl: Set<any> = new Set();
-        let pSet: Set<any> = setOfAllSubsets(eventList);
-        console.log("size of pSet: ", pSet.size);
-
-        // filter out subsets with empty value
-        pSet.forEach((e: any) => {
-            if (e.size == 0) {
-                pSet.delete(e);
-            }
-        });
-
-        // convert to arr
-        let pSetArr: Array<any> = _.toArray(pSet);
-
-        for (let i = 0; i < pSetArr.length; i++) {
-            let first: Set<string> = pSetArr[i];
-            for (let j = 0; j < pSetArr.length; j++) {
-                if (i == j) {
-                    continue;
-                }
-
-                let second: Set<string> = pSetArr[j];
-                if (footprint.areEventsConnected(first, second)) {
-                    let toAdd: Set<string>[] = [first, second];
-                    xl.add(toAdd)
-                }
-            }
-        }
-        console.log("size of xl: ", xl.size);
-        return xl;
-    }
-
-    private reduceXl(xl: Set<Place>): Array<string> {
-        let toRemove: Set<any> = new Set();
-        let potential: Array<any> = _.toArray(xl);
-
-        for (let i = 0; i < potential.length - 1; i++) {
-            let iPotential: Set<string>[] = potential[i];
-            let iInEvents: any = _.toArray(iPotential[0]);
-            let iOutEvents: any = _.toArray(iPotential[1]);
-
-            for (let j = i + 1; j < potential.length; j++) {
-                let jPotential: Set<string>[] = potential[j];
-                let jInEvents: any = _.toArray(jPotential[0]);
-                let jOutEvents: any = _.toArray(jPotential[1]);
-                if (containsAll(iInEvents, jInEvents)) {
-                    if (containsAll(iOutEvents, jOutEvents)) {
-                        toRemove.add(jPotential);
-                        continue;
-                    }
-                }
-
-                if (containsAll(jInEvents, iInEvents)) {
-                    if (containsAll(jOutEvents, iOutEvents)) {
-                        toRemove.add(iPotential);
-                    }
-                }
-            }
-        }
-
-        let toRemoveArr: Array<any> = _.toArray(toRemove);
-        let yl: Array<any> = _.difference(potential, toRemoveArr);
-        console.log("yl set: ", yl);
-        return yl;
     }
 }
