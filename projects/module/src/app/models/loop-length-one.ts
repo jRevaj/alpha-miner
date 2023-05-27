95a60687fdc4ae1e941d8b6af3c5f3fa84255b33
Juraj Revaj <59011895+jRevaj@users.noreply.github.com>
2023-05-27 15:11:23 +0200

    models.loop-length-one: adding docstrings

--- projects/module/src/app/models/loop-length-one.ts
+++ projects/module/src/app/models/loop-length-one.ts
@@ -1,8 +1,17 @@
+/**
+ * LoopLengthOne class
+ */
 export class LoopLengthOne {
     public prevEvent: string | undefined;
     public loopedEvent: string | undefined;
     public nextEvent: string | undefined;
 
+    /**
+     * Creates a new LoopLengthOne object.
+     * @param prevEvent - previous event
+     * @param loopedEvent - looped event
+     * @param nextEvent - next event
+     */
     constructor(prevEvent: string, loopedEvent: string, nextEvent: string) {
         this.prevEvent = prevEvent;
         this.loopedEvent = loopedEvent;
